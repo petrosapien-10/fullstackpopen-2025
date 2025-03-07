80f8468a7b8873f5e70254df5a61588de5f6ac87
Long Nguyen <nhutlongnguyen.work@gmail.com>
2025-03-07 12:19:42 +0200

    completed courseinfo ex 1.1 and 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,32 @@
+const Header = (props) => {
+  return <div> {props.course} </div>;
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <div>
+        {props.part1} {props.exercise1}
+      </div>
+      <div>
+        {props.part2} {props.exercise2}
+      </div>
+      <div>
+        {props.part3} {props.exercise3}
+      </div>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      {" "}
+      Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}
+    </div>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -9,17 +38,20 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercise1={exercises1}
+        part2={part2}
+        exercise2={exercises2}
+        part3={part3}
+        exercise3={exercises3}
+      />
+      <Total
+        exercise1={exercises1}
+        exercise2={exercises2}
+        exercise3={exercises3}
+      />
     </div>
   );
 };
